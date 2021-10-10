aa9f683756015986a5f722e2dd5522848e7b8678
LilChiChi <32337207+AngelGutierrez1990@users.noreply.github.com>
2021-10-10 23:13:04 +0100

    Create Full Stack Engineer - Intro to Js 1 .js

---
+++ Full Stack Engineer - Intro to Js 1 .js
@@ -0,0 +1,46 @@
+Using the console log
+
+console.log(30)
+console.log(0)
+
+// Opening line
+console.log('It was love at first sight.');
+consolel.log(/*IGNORED!*/)
+console.log('The first time Yossarian saw the chaplain he fell madly in love with him.');
+console.log('Yossarian was in the hospital with a pain in his liver that fell just short of being jaundice.');
+console.log('The doctors were puzzled by the fact that it wasn\'t quite jaundice.');
+console.log('If it became jaundice they could treat it.');
+console.log('If it didn\'t become jaundice and went away they could discharge him.');
+console.log('But this just being short of jaundice all the time confused them.');
+
+console.log('JavaScript Location of Codeacademy headquarters: 575 Boradway, New York City');
+console.log(2011);
+console.log('Woohoo! I love to code! #codecademy');
+console.log(20.49);
+
+console.log(30 + 3.5); // plus
+console.log(2021 - 1969); // subtract
+console.log(65 / 240); // divide 
+console.log(0.2708 * 100); // multiply
+
+console.log('Hello' + 'World'); 
+console.log('Hello' +' '+ 'World')
+
+console.log('Teaching the world how to code'.length); //Prints 30
+
+console.log('Codecademy'.toUpperCase()); // Use .toUpperCase() to log 'Codecademy' in all uppercase letters
+console.log('    Remove whitespace   '.trim()); // Use a string method to log the following string without whitespace at the beginning and end of it.
+
+/* Review
+Let’s take one more glance at the concepts we just learned:
+
+Data is printed, or logged, to the console, a panel that displays messages, with console.log().
+We can write single-line comments with // and multi-line comments between /* and */.
+/* There are 7 fundamental data types in JavaScript: strings, numbers, booleans, null, undefined, symbol, and object.
+Numbers are any number without quotes: 23.8879
+Strings are characters wrapped in single or double quotes: 'Sample String'
+The built-in arithmetic operators include +, -, *, /, and %.
+Objects, including instances of data types, can have properties, stored information. The properties are denoted with a . after the name of the object, for example: 'Hello'.length.
+Objects, including instances of data types, can have methods which perform actions. Methods are called by appending the object or instance with a period, the method name, and parentheses. For example: 'hello'.toUpperCase().
+We can access properties and methods by using the ., dot operator.
+Built-in objects, including Math, are collections of methods and properties that JavaScript provides. */ 
