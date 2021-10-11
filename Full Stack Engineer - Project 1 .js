89d2efbda2852656a209838bed3305773c196552
LilChiChi <32337207+AngelGutierrez1990@users.noreply.github.com>
2021-10-11 17:11:35 +0100

    Update Full Stack Engineer - Project 1 .js
    Converted fahrenheit to newtons

--- Full Stack Engineer - Project 1 .js
+++ Full Stack Engineer - Project 1 .js
@@ -9,4 +9,10 @@ let fahrenheit = celsius * (9 / 5) + 32; // Convert celsius to fahrenheit
 fahrenheit = Math.floor(fahrenheit); // Round down fahrenheit, otherwise it'll be decimal points
 console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`);
 
+// Converted fahrenheit to newtons
 
+const kelvin = 0; // Create a constant value for kevlin
+const celsius = kelvin - 273; // Celsius is always 273 degrees lower than kelvin so create a variable for celsius
+let newton = celsius * (33 / 100); // Convert celsius to newton
+newton = Math.floor(newton); // Round down newton, otherwise it'll be decimal points
+console.log(`The temperature is ${newton} degrees Newton.`);
