800d0517851b929b1db28a692965944477bbd63f
LilChiChi <32337207+AngelGutierrez1990@users.noreply.github.com>
2021-10-14 23:19:49 +0100

    Update and rename Full Stack Engineer -  JavaScript Syntax, Part I to Full Stack Engineer -  JavaScript Syntax, Part I .js

--- Full Stack Engineer -  JavaScript Syntax, Part I .js
+++ Full Stack Engineer -  JavaScript Syntax, Part I .js
@@ -92,4 +92,90 @@ let myPet = 'armadillo';
 console.log('I own a pet ' + myPet + '.'); // Output: 'I own a pet armadillo.'
 
 
+// if, else if, and else statements
+
+// Simple if statement
+
+let sale = true;
+sale = false // Once this statement is added the code will not execute as it's now false 
+ if (sale) {
+   console.log('Time to buy!');
+   // Code to execute if the provided condition evaluates to true
+ }
+ 
+ // Else statement Example below
+
+if (false) {
+  console.log('The code in this block will not run.');
+} else {
+  console.log('But the code in this block will!');
+}
+// Prints: But the code in this block will!
+
+// Simple else statement
+
+let sale = true;
+
+sale = false;
+
+if(sale) {
+  console.log('Time to buy!');
+} else {
+  console.log('Time to wait for a sale.')
+}
+
+
+// Comparison Operators
+/*
+Less than: <
+Greater than: >
+Less than or equal to: <=
+Greater than or equal to: >=
+Is equal to: ===
+Is not equal to: !==
+*/ 
+
+let hungerLevel = 8; // Set hunger level
+
+if (hungerLevel > 7) { // set if statement with comparison >
+  console.log('Time to eat!');
+}  
+else { // simple else statement, nothing else needed
+  console.log('We can eat later!');
+}
+
+// Logical Operators
+/* the and operator (&&)
+the or operator (||)
+the not operator, otherwise known as the bang operator (!)
+*/
+
+// Example of a stoplight
+
+if (stopLight === 'green' && pedestrians === 0) {
+  console.log('Go!');
+} else {
+  console.log('Stop');
+}
+
+
+// Example of weekend
+
+if (day === 'Saturday' || day === 'Sunday') {
+  console.log('Enjoy the weekend!');
+} else {
+  console.log('Do some work.');
+}
+
+
+
+let mood = 'sleepy'; // setting mood
+let tirednessLevel = 7; // setting tiredness 
+
+if (mood === 'sleepy' && tirednessLevel > 8) // bunching two if statements
+{ 
+  console.log('time to sleep')
+} else {
+  console.log('not bed time yet')
+}
 
